3608e54e5cc93afc5b468cf5e178b832aae98b3f
tommtollia <tommynguyenfoto@gmail.com>
2022-08-20 03:01:34 +0200

    v3.4 Header + banner + slide + photo + content + client case

---
+++ main.js
@@ -0,0 +1,37 @@
+const menuBtn = document.querySelector('.menu-btn');
+const navMenu = document.querySelector('.nav-menu');
+let menuOpen = false;
+let navopen = false;
+
+
+
+menuBtn.addEventListener('click',()=>{
+    if(!menuOpen){
+        menuBtn.classList.add('open');
+        menuOpen=true;
+    } else {
+        menuBtn.classList.remove('open');
+        menuOpen = false;
+    }
+
+    if(!navopen){
+        navMenu.classList.add('open');
+        navopen=true;
+    } else {
+        navMenu.classList.remove('open');
+        navopen = false;
+    }
+});
+
+document.querySelectorAll(".nav-link,.img-logo,.img-logo_tx").forEach(n=>n.
+addEventListener("click",()=>{
+    menuBtn.classList.remove("open");
+    navMenu.classList.remove("open");
+}))
+window.addEventListener("scroll",function(){
+    const header = document.querySelector(".container-fluid");
+    const navHeader = document.querySelector('.navbarheader');
+    header.classList.toggle("bg-primary", window.scrollY > 0);
+    navHeader.classList.toggle("sticky", window.scrollY > 0);
+    navMenu.classList.toggle("sticky", window.scrollY > 0);
+})
